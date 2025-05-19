a1768a31d6517f195e005a420ef2343234e678db
Raj Vekeria <rvekeria678@gmail.com>
2025-05-19 18:16:11 -0400

    File Renaming

--- 02_agents/single_agent_prebuilt.py
+++ 02_agents/single_agent_prebuilt.py
@@ -2,23 +2,21 @@ from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 
-# ----- TOOL DEFINITION ----- #
+# Define a tool
 @tool
 def magic_function(input: int) -> int:
     """function returns double the input."""
     print(">:(magic_function invoked)")
     return input * 2
 
-# ----- LLM SETUP ----- #
+# LLM setup
 model = ChatOpenAI(model="gpt-3.5-turbo")
 tools = [magic_function]
 
-# ----- AGENT SETUP ----- #
-react_agent = create_react_agent(
-    model=model,
-    tools=tools
-)
+# Create the agent
+react_agent = create_react_agent(model, tools)
 
-# ----- AGENT EXECUTION ----- #
+# Use the agent
 for step in react_agent.stream({"messages":[("human", "what is the value of magic_function(3)?")]}, stream_mode="updates"):
-    print(step['messages'])
+    for key, value, in step.items():
+        print(f"{key}: {value}")
